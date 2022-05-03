0c3a4f8e8c3ca0ba1ffcf40b53c6493f4b6f0f56
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-05-03 13:28:53 -0400

    gh readme

--- src/Footer.js
+++ src/Footer.js
@@ -21,7 +21,7 @@ export default class Footer extends React.Component {
                     </a>
                 </p>
                 <div>
-                    <a href="https://github.com/codeTrackLift/markdown-previewer" target="_blank" rel='noreferrer'>
+                    <a href="https://github.com/codeTrackLift/markdown-previewer#readme`" target="_blank" rel='noreferrer'>
                         <img className="socialLogo" src="https://codetracklift.github.io/codeTrackLift/logos/GitHub-Mark-Light-64px.png" alt="GitHub Icon"/>
                     </a>
                     <a href="https://twitter.com/codetracklift" target="_blank" rel='noreferrer'>
@@ -35,4 +35,3 @@ export default class Footer extends React.Component {
         )
     }
 }
-
